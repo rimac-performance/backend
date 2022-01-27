5b7344e24971caa2b9a48bbfabdd606ff06817d4
Ezana <eek9045@rit.edu>
2022-01-27 12:39:58 -0500

    Setting up env variables

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8080
 const cors = require("cors")
+require('dotenv').config()
 const { validationResult } = require('express-validator');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get("/hello", (req, res) => {
-    res.send("hi");
+    res.send("hi: " + process.env.db_username);
 })
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`)
+    console.log(process.env.db_username)
 })
